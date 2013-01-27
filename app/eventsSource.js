80f12d1c94717e99db79916ce4fee3b87bfdfc82
Vagrant <vagrant@vagrant-ubuntu.vagrantup.com>
2013-01-27 07:13:23 -0200

    update

--- app/eventsSource.js
+++ app/eventsSource.js
@@ -1,11 +1,23 @@
-define(function(){
-	
-	$.ajax("/published.conf", {
-		success: function(data){
-			_.each(data.split(/\n/), function(line){
-				line.split(" ")[0];
-			});
-		}
-	});
-	return {};
+require(["../libs/text!/published.conf"],function(published){
+	var eventSource = {};
+	_.extend(eventSource, Backbone.Events);
+
+	_.each(published.split(/\n/), eachPublishedEvent);
+
+	function eachPublishedEvent(line){
+		var eventUrl = line.split(" ")[0];
+		$.ajax(eventUrl, {
+			success: gotEventData,
+			error: notFound
+		});
+	}
+	function gotEventData(data){
+		eventSource.trigger("addEvent", data);
+	}
+
+	function notFound(){
+		//console.log(arguments);
+	}
+
+	return eventSource;
 });
